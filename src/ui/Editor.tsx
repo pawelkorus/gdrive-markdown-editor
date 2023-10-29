c38f76c7b6a8b5cfe9320c29480847a4ef63be9d
Pawel Korus <pawelkorus@ymail.com>
2023-10-29 23:52:15 +0100

    styling editor

--- src/ui/Editor.tsx
+++ src/ui/Editor.tsx
@@ -30,26 +30,21 @@ export default function(props:Props):React.ReactElement {
     }
 
     return (
-<div>
-    <div className="container-fluid p-2">
-        <div className="d-flex flex-row">
-            <div className="me-auto me-2"></div>
-            <button className="btn btn-primary ms-1" id="btn-save" type="button" onClick={e => props.onSaveClicked(new SaveEvent(updatedContent))}>Save</button>
-            <button className="btn btn-primary ms-1" id="btn-browse" type="button" onClick={browseGdrive}>Browse</button>
-            <button className="btn btn-primary ms-1" id="btn-preview" type="button" onClick={togglePreview}>Preview</button>
-            <button className="btn btn-primary ms-1" id="btn-close" type="button" onClick={props.onCloseClicked}>Close</button>
-        </div>
+<div className="container-fluid p-2 d-flex flex-column min-vh-100">
+    <div className="flex-row">
+        <div className="me-auto me-2"></div>
+        <button className="btn btn-primary ms-1" id="btn-save" type="button" onClick={e => props.onSaveClicked(new SaveEvent(updatedContent))}>Save</button>
+        <button className="btn btn-primary ms-1" id="btn-browse" type="button" onClick={browseGdrive}>Browse</button>
+        <button className="btn btn-primary ms-1" id="btn-preview" type="button" onClick={togglePreview}>Preview</button>
+        <button className="btn btn-primary ms-1" id="btn-close" type="button" onClick={props.onCloseClicked}>Close</button>
     </div>
-    <div className="container-lg mt-4" id="main">
-        <div className="row content"> 
-            <div className="col">
-                    <textarea className="form-control w-100 h-100" 
-                        value={updatedContent} 
-                        onChange={e => { setUpdatedContent(e.target.value) }}></textarea>
-            </div>
-
-            {previewEnabled && <div className="col"><MarkdownContent content={updatedContent}/></div>}    
+    <div className="d-flex flex-row flex-fill mt-4">
+        <div className="position-relative" style={{flex: 1}}>
+            <textarea className="form-control position-absolute" style={{minHeight: "100%"}}
+                value={updatedContent} 
+                onChange={e => { setUpdatedContent(e.target.value) }}></textarea>
         </div>
+        {previewEnabled && <div style={{flex: 1}} className="p-2"><MarkdownContent content={updatedContent}/></div>}  
     </div>
 </div>
 )}
