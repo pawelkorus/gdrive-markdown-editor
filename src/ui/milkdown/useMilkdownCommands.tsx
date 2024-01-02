64bfe27c8bcaeaa80d5a9c725671e9c791b40117
Pawel Korus <pawelkorus@ymail.com>
2024-01-02 09:18:43 +0000

    command palette: additional commands

--- src/ui/milkdown/useMilkdownCommands.tsx
+++ src/ui/milkdown/useMilkdownCommands.tsx
@@ -5,62 +5,86 @@ import {
     turnIntoTextCommand,
     createCodeBlockCommand,
     insertHrCommand,
+    toggleEmphasisCommand,
+    toggleStrongCommand,
+    toggleLinkCommand,
+    wrapInBlockquoteCommand
   } from "@milkdown/preset-commonmark";
 import { gdriveCommand } from "./gdrive-plugin";
 import { commandsCtx } from '@milkdown/core';
+import type { $Command } from '@milkdown/utils';
 
 export default function useMilkdownCommands() {
     const [loading, getInstance] = useInstance();
 
+    function callCommand<T>(cmd: $Command<T>, payload?: T) {
+        return () => getInstance().ctx.get(commandsCtx).call(cmd.key, payload);
+    }
+
     const commands:Command[] = [{
             id: "heading1",
-            execute: () => getInstance().ctx.get(commandsCtx).call(wrapInHeadingCommand.key, 1),
+            execute: callCommand(wrapInHeadingCommand, 1),
             name: "Heading 1"
         },
         {
             id: "heading2",
-            execute: () => getInstance().ctx.get(commandsCtx).call(wrapInHeadingCommand.key, 2),
+            execute: callCommand(wrapInHeadingCommand, 2),
             name: "Heading 2"
         },
         {
             id: "heading3",
-            execute: () => getInstance().ctx.get(commandsCtx).call(wrapInHeadingCommand.key, 3),
+            execute: callCommand(wrapInHeadingCommand, 3),
             name: "Heading 3"
         },
         {
             id: "heading4",
-            execute: () => getInstance().ctx.get(commandsCtx).call(wrapInHeadingCommand.key, 4),
+            execute: callCommand(wrapInHeadingCommand, 4),
             name: "Heading 4"
         },
         {
             id: "heading5",
-            execute: () => getInstance().ctx.get(commandsCtx).call(wrapInHeadingCommand.key, 5),
+            execute: callCommand(wrapInHeadingCommand, 5),
             name: "Heading 5"
         },
         {
             id: "heading6",
-            execute: () => getInstance().ctx.get(commandsCtx).call(wrapInHeadingCommand.key, 6),
+            execute: callCommand(wrapInHeadingCommand, 6),
             name: "Heading 6"
         },
         {
             id: "turnIntoText",
-            execute: () => getInstance().ctx.get(commandsCtx).call(turnIntoTextCommand.key),
+            execute: callCommand(turnIntoTextCommand),
             name: "Turn into text"
         },
         {
             id: "codeblock",
-            execute: () => getInstance().ctx.get(commandsCtx).call(createCodeBlockCommand.key),
+            execute: callCommand(createCodeBlockCommand),
             name: "Code block"
         },
         {
+            id: "blockquote",
+            execute: callCommand(wrapInBlockquoteCommand),
+            name: "Blockquote"
+        },
+        {
             id: "hr",
-            execute: () => getInstance().ctx.get(commandsCtx).call(insertHrCommand.key),
+            execute: callCommand(insertHrCommand),
             name: "Horizontal divider"
         },
         {
             id: "gdrive",
-            execute: () => getInstance().ctx.get(commandsCtx).call(gdriveCommand.key),
+            execute: callCommand(gdriveCommand),
             name: "Insert image from google drive"
+        },
+        {
+            id: "emphasize",
+            execute: callCommand(toggleEmphasisCommand),
+            name: "Toggle emphasize"
+        },
+        {
+            id: "strong",
+            execute: callCommand(toggleStrongCommand),
+            name: "Toggle strong"
         }
     ];
 
