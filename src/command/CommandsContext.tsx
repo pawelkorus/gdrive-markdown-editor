db2f3830251393b508ae7ac8ebff34033d0de77f
Pawel Korus <pawelkorus@ymail.com>
2024-01-03 01:18:23 +0000

    commandPalette: make global

--- src/command/CommandsContext.tsx
+++ src/command/CommandsContext.tsx
@@ -1,49 +1,64 @@
-import React, { PropsWithChildren, createContext } from 'react';
+import React, { PropsWithChildren, createContext, useCallback, useState } from 'react';
 import { Command } from './Command';
 
 type CommandsRegistry = Record<string, Command>
 
 export type CommandsContextState = {
-    registerCommand: (command:Command) => void
-    unregisterCommand: (command:Command) => void
+    commands: Command[]
+    registerCommand: (command:Command|Command[]) => void
+    unregisterCommand: (command:Command|Command[]) => void
     executeCommand: (commandId:string) => void
-    getCommands: () => Command[]
 }
 
 export const CommandsContext = createContext<CommandsContextState>({
+    commands: [],
     registerCommand: () => { throw Error("CommandsContext not initialized"); },
     unregisterCommand: () => { throw Error("CommandsContext not initialized"); },
-    executeCommand: () => { throw Error("CommandsContext not initialized"); },
-    getCommands: () => { throw Error("CommandsContext not initialized"); }
+    executeCommand: () => { throw Error("CommandsContext not initialized"); }
 });
 
 export type Props = PropsWithChildren<unknown>
 
 export function CommandsContextProvider(props:Props):React.ReactElement {
-    const commandRegistry:CommandsRegistry = {};
-    const state = {getCommands, registerCommand, unregisterCommand, executeCommand } as CommandsContextState;
+    const [commandRegistry, setCommandRegistry] = useState<CommandsRegistry>({});
+    const registerCommand = useCallback((command:Command|Command[]) => {
+        if(!Array.isArray(command)) {
+            command = [command]
+        }
+        const additionalCommands:CommandsRegistry = {};
+        command.forEach(c => {
+            if(!commandRegistry[c.id]) additionalCommands[c.id] = c;
+        });
+        
+        if(Object.values(additionalCommands).length > 0) {
+            console.log("Registering commands", additionalCommands);
+            setCommandRegistry({...commandRegistry, ...additionalCommands});
+        }
+    }, [commandRegistry]);
+    const unregisterCommand = useCallback((command:Command|Command[]) => {
+        if(!Array.isArray(command)) {
+            command = [command]
+        }
+        command.forEach(c => { delete commandRegistry[c.id]; });
+        setCommandRegistry({...commandRegistry});
+    }, [commandRegistry]);
 
-    function getCommands() {
-        return Object.values(commandRegistry) as Command[];
-    }
-
-    function registerCommand(command:Command) {
-        commandRegistry[command.id] = command;
-    }
-
-    function unregisterCommand(command:Command) {
-        delete commandRegistry[command.id];
-    }
-
-    function executeCommand(commandId:string) {
+    const executeCommand = useCallback((commandId:string) => {
         const command = commandRegistry[commandId];
         if(command) {
             command.execute();
         }
-    }
+    }, [commandRegistry]);
+
+    const value:CommandsContextState = {
+        commands: Object.values(commandRegistry),
+        registerCommand,
+        unregisterCommand,
+        executeCommand
+    };
 
     return (
-        <CommandsContext.Provider value={state}>
+        <CommandsContext.Provider value={value}>
             {props.children}
         </CommandsContext.Provider>
     )
