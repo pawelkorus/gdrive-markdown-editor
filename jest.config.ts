fb85e6c0e3ed5ca78bb5c4fa6a76df959e854475
Pawel Korus <pawelkorus@ymail.com>
2024-01-03 23:56:41 +0000

    CommandPalette: extending tests

---
+++ jest.config.ts
@@ -0,0 +1,10 @@
+import type { Config } from 'jest';
+
+const config:Config = {
+    preset: 'ts-jest',
+    testEnvironment: 'jsdom',
+    setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+    injectGlobals: true
+}
+
+export default config;
--- test/ui/commandPalette/CommandPalette.test.tsx
+++ test/ui/commandPalette/CommandPalette.test.tsx
@@ -1,17 +1,74 @@
 import React from 'react';
-import renderer from 'react-test-renderer';
-// import { render, fireEvent } from '@testing-library/react';
+import { render, fireEvent, screen, waitForElementToBeRemoved } from '@testing-library/react';
 import CommandPalette from '../../../src/ui/commandPalette/CommandPalette';
-import { test, expect } from '@jest/globals';
 
-test('renders hidden', () => {
+function prepareCommands() {
     const commands = [{
-        id: 'some-command',
-        name: 'Some Command',
+        id: 'aa',
+        name: 'aa',
+        execute: () => {}
+    },
+    {
+        id: 'ab',
+        name: 'ab',
+        execute: () => {}
+    },
+    {
+        id: 'bb',
+        name: 'bb',
         execute: () => {}
     }];
-    
-    const component = renderer.create(<CommandPalette commands={commands}/>).toJSON();
 
-    expect(component).toMatchSnapshot();
+    return commands;
+}
+
+test('command palette opens after pressing /', () => {   
+    render(<CommandPalette commands={prepareCommands()}/>);
+
+    expect(document.body).toMatchSnapshot();
+
+    fireEvent.keyDown(document, { key: '/' });
+
+    expect(document.body).toMatchSnapshot();
+})
+
+test('should execute callback after command is selected by keyboard', () => {
+    const mockOnSelectCallback = jest.fn();
+
+    render(<CommandPalette commands={prepareCommands()} onItemSelected={mockOnSelectCallback}/>);
+
+    fireEvent.keyDown(document, { key: '/' });
+    fireEvent.keyDown(document, { key: 'ArrowDown' });
+    fireEvent.keyDown(document, { key: 'Enter' });
+
+    expect(mockOnSelectCallback).toHaveBeenCalledWith({id: 'ab', name: 'ab', execute: expect.any(Function)});
+    waitForElementToBeRemoved(() => screen.getByRole('dialog'));
+})
+
+test('should execute callback after command is selected by mouse click', () => {
+    const mockOnSelectCallback = jest.fn();
+
+    render(<CommandPalette commands={prepareCommands()} onItemSelected={mockOnSelectCallback}/>);
+
+    fireEvent.keyDown(document, { key: '/' });
+    fireEvent.mouseDown(screen.getByText('ab'));
+
+    expect(mockOnSelectCallback).toHaveBeenCalledWith({id: 'ab', name: 'ab', execute: expect.any(Function)});
+    waitForElementToBeRemoved(() => screen.getByRole('dialog'));
+})
+
+test('should filter commands based on input value', () => {
+    render(<CommandPalette commands={prepareCommands()} />);
+
+    fireEvent.keyDown(document, { key: '/' });
+
+    const filterInput = screen.getByPlaceholderText('Search...');
+    fireEvent.change(filterInput, { target: { value: 'a' } });
+
+    expect(document.body).toMatchSnapshot();
+
+    fireEvent.change(filterInput, { target: { value: 'aa' } });
+
+    expect(document.body).toMatchSnapshot();
 });
+
