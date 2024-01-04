ba96758bfb4d84fb65e4f84c5b57c1e532cea7f0
Pawel Korus <pawelkorus@ymail.com>
2024-01-04 10:22:46 +0000

    EditorView: simplify and match layout with ViewerView

--- src/ui/EditorView.tsx
+++ src/ui/EditorView.tsx
@@ -62,28 +62,30 @@ function EditorView(props:Props):React.ReactElement {
     }
 
     return (
-<div className="container-fluid p-2 d-flex flex-column min-vh-100">
-    <div className="d-flex flex-row align-items-center justify-content-end">
-        {editFileNameEnabled ? (
-            <input
-            type="text"
-            value={fileName}
-            onChange={e => setFileName(e.target.value)}
-            onBlur={handleFileNameChange}
-            autoFocus
-            className='form-control me-auto'
-            />
-        ) : ( <h5 className='me-auto mb-0' onClick={() => setEditFileNameEnabled(true)}>{fileName}</h5> )}
-        {lastSavedTimestamp != null && <span className="ms-1"><small className="text-success">Last saved at {lastSavedTimestamp.toLocaleString()}</small></span>}
-        <button className="btn btn-primary ms-1" id="btn-save" type="button" onClick={() => save(updatedContent)}>Save</button>
-        <button className="btn btn-primary ms-1" id="btn-close" type="button" onClick={props.onCloseClicked}>Close</button>
+<>
+    <div className="container-fluid p-2">
+        <div className="d-flex flex-row align-items-center justify-content-end">
+            {editFileNameEnabled ? (
+                <input
+                type="text"
+                value={fileName}
+                onChange={e => setFileName(e.target.value)}
+                onBlur={handleFileNameChange}
+                autoFocus
+                className='form-control me-auto'
+                />
+            ) : ( <h5 className='me-auto mb-0' onClick={() => setEditFileNameEnabled(true)}>{fileName}</h5> )}
+            {lastSavedTimestamp != null && <span className="ms-1"><small className="text-success">Last saved at {lastSavedTimestamp.toLocaleString()}</small></span>}
+            <button className="btn btn-primary ms-1" id="btn-save" type="button" onClick={() => save(updatedContent)}>Save</button>
+            <button className="btn btn-primary ms-1" id="btn-close" type="button" onClick={props.onCloseClicked}>Close</button>
+        </div>
     </div>
-    <div className="d-flex flex-row flex-fill mt-4">
-        <div className="position-relative" style={{flex: 1}}>
+    <div className="container-lg mt-4">
+        <div className="row">
             <MilkdownEditor content={props.content} onContentUpdated={updateContent}/>
         </div>
     </div>
-</div>
+</>
 )}
 
 export default function(props:Props):React.ReactElement {
