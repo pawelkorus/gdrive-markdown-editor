c6f8608da4c11578139429164ea3cf4b9309b47f
Pawel Korus <pawelkorus@ymail.com>
2024-01-07 23:40:41 +0000

    inspector-plugin

---
+++ src/ui/milkdown/dev-plugin/index.tsx
@@ -0,0 +1,25 @@
+import { ReactPluginViewUserOptions, usePluginViewFactory } from "@prosemirror-adapter/react";
+import { $prose } from "@milkdown/utils";
+import { PluginView, Plugin } from "@milkdown/prose/state";
+import { EditorView } from "@milkdown/prose/view";
+import MilkdownInspector from "./MilkdownInspector";
+
+function createPlugin(factory:(options: ReactPluginViewUserOptions) => (editorView:EditorView) => PluginView) {
+    return $prose(() => {
+        return new Plugin({
+            view: factory({
+                component: MilkdownInspector,
+            })
+        })
+    });
+}
+
+export const useInspector = () => {
+    const pluginViewFactory = usePluginViewFactory();
+    
+    return {
+        plugins: [
+            createPlugin(pluginViewFactory)
+        ].flat()
+    };
+};
