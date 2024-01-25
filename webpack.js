b089e38fec38ba21b8a45899c5fcdfed98132116
Pawel Korus <pawelkorus@ymail.com>
2024-01-25 22:32:20 +0000

    feat: Format webpack.js, fix webpack.js indentation and add MiniCssExtractPlugin

--- webpack.js
+++ webpack.js
@@ -1,107 +1,114 @@
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require("mini-css-extract-plugin")
-const path = require('path');
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require("path");
 
 module.exports = (env, argv) => {
-    const configDir = env.local? '.config-local' : '.config-dev'
-    const entryFile = './src/index.tsx'
-    
-    return {
-        entry: {
-            'index': entryFile
+  const configDir = env.local ? ".config-local" : ".config-dev";
+  const entryFile = "./src/index.tsx";
+
+  return {
+    entry: {
+      index: entryFile,
+    },
+    devtool: "source-map",
+    devServer: {
+      host: "0.0.0.0",
+      server: "https",
+      static: [
+        path.resolve(__dirname, "target"),
+        path.resolve(__dirname, configDir),
+      ],
+      port: 8080,
+      allowedHosts: "all",
+    },
+    optimization: {
+      splitChunks: {
+        cacheGroups: {
+          commons: {
+            test: /[\\/]node_modules[\\/]/,
+            name: "vendor",
+            chunks: "initial",
+          },
         },
-        devtool: 'source-map',
-        devServer: {
-            server: 'https',
-            static: [path.resolve(__dirname, 'target'), path.resolve(__dirname, configDir)],
-            port: 8080,
-            allowedHosts: 'all',
+      },
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(tsx|ts)?$/,
+          use: "ts-loader",
+          exclude: /node_modules/,
         },
-        optimization: { 
-            splitChunks: {
-                cacheGroups: {
-                    commons: {
-                        test: /[\\/]node_modules[\\/]/,
-                        name: "vendor",
-                        chunks: "initial",
-                    },
-                }, 
+        {
+          test: /\.(css)/,
+          use: [
+            MiniCssExtractPlugin.loader, // instead of style-loader
+            {
+              loader: "css-loader", // translates CSS into CommonJS modules
             },
+            {
+              loader: "postcss-loader",
+            },
+          ],
         },
-        module: {
-            rules: [
-                {
-                    test: /\.(tsx|ts)?$/,
-                    use: 'ts-loader',
-                    exclude: /node_modules/
-                },
-                {
-                    test: /\.(css)/,
-                    use: [ 
-                        MiniCssExtractPlugin.loader, // instead of style-loader
-                        {
-                            loader: 'css-loader', // translates CSS into CommonJS modules
-                        }, {
-                            loader: 'postcss-loader'
-                        }
-                    ]
-                },
-                {
-                    test: /\.(scss)/,
-                    use: [
-                        MiniCssExtractPlugin.loader, 
-                        {
-                            loader: 'css-loader', // translates CSS into CommonJS modules
-                        }, {
-                            loader: 'postcss-loader'                        
-                        },{
-                            loader: 'sass-loader' // compiles Sass to CSS
-                        }
-                    ]
-                },
-                {
-                  test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
-                  type: 'asset/resource',
-                  generator: {
-                    filename: 'fonts/[hash][ext][query]'
-                  }
-                },
-                {
-                  test: /\.(csv)$/,
-                  type: 'asset/resource'
-                },
-                {
-                  test: /\.(png)$/,
-                  type: 'asset/resource'
-                }
-            ]
+        {
+          test: /\.(scss)/,
+          use: [
+            MiniCssExtractPlugin.loader,
+            {
+              loader: "css-loader", // translates CSS into CommonJS modules
+            },
+            {
+              loader: "postcss-loader",
+            },
+            {
+              loader: "sass-loader", // compiles Sass to CSS
+            },
+          ],
         },
-        resolve: {
-            extensions: [ '.tsx', '.ts', '.js', '.scss', '.json', '.css', '.scss' ],
-            fallback: {
-                'http': false,
-                'crypto': false,
-                'fs': false,
-                'path': false,
-                'os': false,
-                'stream': false,
-                'buffer': false
-            }
+        {
+          test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+          type: "asset/resource",
+          generator: {
+            filename: "fonts/[hash][ext][query]",
+          },
         },
-        output: {
-            filename: '[name].bundle.[contenthash].js',
-            path: path.resolve(__dirname, 'target')
+        {
+          test: /\.(csv)$/,
+          type: "asset/resource",
         },
-        plugins: [
-            new MiniCssExtractPlugin(),
-            new CleanWebpackPlugin(),
-            new HtmlWebpackPlugin({
-              inlineSource: '.(js|css)$', // embed all javascript and css inline
-              chunks: ['index'],
-              template: 'src/index.html',
-              filename: 'index.html'
-            })
-        ]
-    }
-}
+        {
+          test: /\.(png)$/,
+          type: "asset/resource",
+        },
+      ],
+    },
+    resolve: {
+      extensions: [".tsx", ".ts", ".js", ".scss", ".json", ".css", ".scss"],
+      fallback: {
+        http: false,
+        crypto: false,
+        fs: false,
+        path: false,
+        os: false,
+        stream: false,
+        buffer: false,
+      },
+    },
+    output: {
+      filename: "[name].bundle.[contenthash].js",
+      path: path.resolve(__dirname, "target"),
+    },
+    plugins: [
+      new MiniCssExtractPlugin(),
+      new CleanWebpackPlugin(),
+      new HtmlWebpackPlugin({
+        inlineSource: ".(js|css)$", // embed all javascript and css inline
+        chunks: ["index"],
+        template: "src/index.html",
+        filename: "index.html",
+      }),
+    ],
+  };
+};
