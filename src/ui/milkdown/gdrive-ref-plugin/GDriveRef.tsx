9eba9433407688c492d06fba5b0f477001861748
Pawel Korus <pawelkorus@ymail.com>
2024-05-13 00:58:59 +0200

    gdrive-ref-plugin - openning md files directly in editor

--- src/ui/milkdown/gdrive-ref-plugin/GDriveRef.tsx
+++ src/ui/milkdown/gdrive-ref-plugin/GDriveRef.tsx
@@ -1,16 +1,25 @@
-import React from 'react'
+import React, { useCallback } from 'react'
 import { useNodeViewContext } from '@prosemirror-adapter/react'
 import { useGdriveFileMetadata } from '../../../service/gdrivefiles'
+import { useGdriveFile } from '../../../service/gdrivefile'
 
 export default function (): React.ReactElement {
   const { contentRef, node } = useNodeViewContext()
+  const [, loadFile] = useGdriveFile()
   const fileMetadata = useGdriveFileMetadata(node.attrs.src)
+
+  const handleClick = useCallback((e: React.MouseEvent) => {
+    if (fileMetadata && (fileMetadata.mimeType === 'text/markdown' || fileMetadata.name.endsWith('.md'))) {
+      e.preventDefault()
+      loadFile(node.attrs.src)
+    }
+  }, [loadFile, fileMetadata, node])
 
   return fileMetadata
     ? (
-      <a data-type="gdrive-ref" data-src={node.attrs.src} href={fileMetadata.url} ref={contentRef}>
+      <a data-type="gdrive-ref" data-src={node.attrs.src} href={fileMetadata.url} ref={contentRef} onClick={handleClick}>
         {fileMetadata.name}
       </a>
       )
-    : null
+    : <span>Loading...</span>
 }
