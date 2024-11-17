117c0fd8ef05800993aaa48c63be361b61f7d344
Pawel Korus <pawelkorus@ymail.com>
2024-11-17 16:30:21 +0000

    viewer: edit source button

--- src/ui/ViewerView.tsx
+++ src/ui/ViewerView.tsx
@@ -8,11 +8,16 @@ import { useMainMenuSlot } from '@app/service/navbar'
 function ViewerView(): React.ReactElement {
   const [fileDetails] = useGdriveFile()
   const { addPanel } = useMainMenuSlot()
-  const { navigateToFileEdit } = useNavigateTo()
+  const { navigateToFileEdit, navigateToFileSource } = useNavigateTo()
 
   return (
     <div className="container-lg mt-4">
-      {addPanel(<Panel><PanelButton onClick={() => navigateToFileEdit()}>Edit</PanelButton></Panel>)}
+      {addPanel(
+        <Panel>
+          <PanelButton onClick={() => navigateToFileEdit()}>Edit</PanelButton>
+          <PanelButton onClick={() => navigateToFileSource()}>Edit source</PanelButton>
+        </Panel>,
+      )}
       <div className="row">
         <MilkdownEditor content={fileDetails.content} readonly={true} />
       </div>
